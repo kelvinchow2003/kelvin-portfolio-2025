1a98b52895bcd4b61f714bae6f5fe7aa5eaadef8
kelvinchow2003 <kelvinchow2014@gmail.com>
2025-11-05 11:39:14 -0500

    git pages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// 💡 IMPORTANT: Replace 'REPOSITORY-NAME' with your actual GitHub repository name, 
-// including the leading and trailing slashes. E.g., '/kelvin-portfolio/'
-const repositoryName = '/Kelvins-Website-2025/'
+// Must match your repo path exactly:
+const repositoryName = '/kelvin-portfolio-2025/'
 
 export default defineConfig({
-  // --- ADDED THIS LINE ---
-  base: repositoryName, 
-  // -----------------------
+  base: repositoryName,
   plugins: [react(), tailwindcss()],
 })
