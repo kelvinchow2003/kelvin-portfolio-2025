c692fc9c032ec606a71a4b4225799672ea65c863
kelvinchow2003 <kelvinchow2014@gmail.com>
2025-11-04 19:28:39 -0500

    fix vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 // vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'          // if you're using React
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// 💡 IMPORTANT: Replace 'REPOSITORY-NAME' with your actual GitHub repository name, 
+// including the leading and trailing slashes. E.g., '/kelvin-portfolio/'
+const repositoryName = '/https://github.com/kelvinchow2003/Kelvins-Website-2025-NAME/' 
+
 export default defineConfig({
+  // --- ADDED THIS LINE ---
+  base: repositoryName, 
+  // -----------------------
   plugins: [react(), tailwindcss()],
 })
