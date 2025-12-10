eb6867e657a623cd59bc0e4f7063d8bfa48e3d6b
kelvinchow2003 <kelvinchow2014@gmail.com>
2025-12-10 12:07:59 -0500

    Renamed Repository and updated config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // Must match your repo path exactly:
-const repositoryName = '/kelvin-portfolio-2025/'
+const repositoryName = '/k/'
 
 export default defineConfig({
   base: repositoryName,
